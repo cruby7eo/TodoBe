4bd2bc536b9b5c424081e649a965116fee4f5056
yoobin <cruby7eo@gmail.com>
2024-10-18 18:20:32 +0900

    first version

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const indexRouter = require('./routes/index');
+require('dotenv').config();
+const cors = require('cors');
+
+
+const app = express()
+
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use('/api', indexRouter)
+
+const mongoURI = MONGODB_URI_PROD
+
+mongoose
+    .connect(mongoURI, {useNewUrlParser: true})
+    .then(()=>{
+        console.log("mongoose connected");
+        
+    })
+    .catch((err)=>{
+        console.log("db is not working");
+        
+    })
+    
+app.get('/', function (req, res) {
+    res.send('hello world')
+})
+
+app.get('/about', function (req, res) {
+    res.send('this is allabout express')
+})
+
+app.listen(5000, ()=>{
+    console.log("server is on 5000");
+    
+})
